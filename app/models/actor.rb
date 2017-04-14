6b5070a7cd3a10ab6d3020daccd7279689a14cea
kylewstewart <kyle.w.stewart@mac.com>
2017-04-14 15:32:51 -0400

    Completed activerecord-tvland-web-040317

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  # binding.pry
+
+  def full_name
+    name = "#{self.first_name} #{self.last_name}"
+    name
+  end
+
+  def list_roles
+    self.characters.map do |x|
+      "#{x.name} - #{x.show.name}"
+    end
+  end
+
+
 end
